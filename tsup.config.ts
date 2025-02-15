b8b20141b4ac94fc5eaeae785d52d2e87f0fb9b2
w4jo <stankovic.jovan.gg@gmail.com>
2025-02-15 12:36:59 +0100

    chore: minor settings changes

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,15 +4,20 @@ export default defineConfig({
   entry: ['src/components/ui/*'],
   outDir: 'dist',
   format: ['esm'],
-  dts: true,
+  dts: {
+    resolve: true,
+    compilerOptions: {
+      moduleResolution: 'node',
+      declarationDir: './dist',
+    },
+  },
   clean: true,
   treeshake: true,
   splitting: true,
   skipNodeModulesBundle: true,
   target: 'es2022',
 
-  // Dev
-  // external: ['react', 'react-dom', 'react-hook-form'],
+  external: ['react'],
 
   // Prod
   minify: true,
