eb701fe1e734d30bcbea8b720378fb617408baa2
Jovan <stankovic.jovan.gg@gmail.com>
2025-03-01 05:47:28 +0100

    feat: command and alert dialog style update

--- src/alert-dialog.tsx
+++ src/alert-dialog.tsx
@@ -44,6 +44,7 @@ function AlertDialogOverlay({
 
 function AlertDialogContent({
   className,
+  children,
   ...props
 }: ComponentProps<typeof AlertDialogPrimitive.Content>) {
   return (
@@ -52,11 +53,15 @@ function AlertDialogContent({
       <AlertDialogPrimitive.Content
         data-slot="alert-dialog-content"
         className={cn(
-          'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in sm:max-w-lg',
+          'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border bg-background p-2 shadow-3xl duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in sm:max-w-lg',
           className,
         )}
         {...props}
-      />
+      >
+        <div className="max-h-full w-full min-w-0 rounded-2xl p-6 shadow-dialog backdrop-blur-[1px] transition-all duration-300 will-change-transform">
+          {children}
+        </div>
+      </AlertDialogPrimitive.Content>
     </AlertDialogPortal>
   )
 }
