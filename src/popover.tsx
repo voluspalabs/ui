686f2ca810a73173149a11e98caf67fb568f0ef8
Jovan Stankovic <stankovic.jovan.gg@gmail.com>
2025-09-15 12:00:40 +0200

    chore: update react-day-picker and rollup dependencies to latest versions

--- src/popover.tsx
+++ src/popover.tsx
@@ -34,7 +34,7 @@ function PopoverContent({
   return (
     <PopoverPrimitive.Popup
       className={cn(
-        'data-[closed]:fade-out-0 data-[open]:fade-in-0 data-[closed]:zoom-out-95 data-[open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-hidden data-[closed]:animate-out data-[open]:animate-in',
+        'data-[closed]:fade-out-0 data-[open]:fade-in-0 data-[closed]:zoom-out-95 data-[open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-hidden data-[closed]:animate-out data-[open]:animate-in',
         className,
       )}
       data-slot="popover-content"
