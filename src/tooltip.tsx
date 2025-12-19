c2566271a167545d7946a3b69f4fc1cbf953cb3c
Jovan Stankovic <stankovic.jovan.gg@gmail.com>
2025-12-19 23:43:47 +0100

    feat: add TooltipPortal, TooltipPositioner, TooltipPopup, and TooltipArrow components

--- src/tooltip.tsx
+++ src/tooltip.tsx
@@ -39,6 +39,57 @@ function TooltipTrigger({ ...props }: TooltipPrimitive.Trigger.Props) {
   return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />
 }
 
+function TooltipPortal({ ...props }: TooltipPrimitive.Portal.Props) {
+  return <TooltipPrimitive.Portal data-slot="tooltip-portal" {...props} />
+}
+
+function TooltipPositioner({
+  className,
+  side = 'top',
+  sideOffset = 4,
+  align = 'center',
+  alignOffset = 0,
+  ...props
+}: TooltipPrimitive.Positioner.Props) {
+  return (
+    <TooltipPrimitive.Positioner
+      align={align}
+      alignOffset={alignOffset}
+      className={cn('isolate z-50', className)}
+      data-slot="tooltip-positioner"
+      side={side}
+      sideOffset={sideOffset}
+      {...props}
+    />
+  )
+}
+
+function TooltipPopup({ className, ...props }: TooltipPrimitive.Popup.Props) {
+  return (
+    <TooltipPrimitive.Popup
+      className={cn(
+        'data-open:fade-in-0 data-open:zoom-in-95 data-[state=delayed-open]:fade-in-0 data-[state=delayed-open]:zoom-in-95 data-closed:fade-out-0 data-closed:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit max-w-xs origin-(--transform-origin) rounded-md bg-foreground px-3 py-1.5 text-background text-xs data-[state=delayed-open]:animate-in data-closed:animate-out data-open:animate-in',
+        className,
+      )}
+      data-slot="tooltip-popup"
+      {...props}
+    />
+  )
+}
+
+function TooltipArrow({ className, ...props }: TooltipPrimitive.Arrow.Props) {
+  return (
+    <TooltipPrimitive.Arrow
+      className={cn(
+        'z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-foreground fill-foreground data-[side=bottom]:top-1 data-[side=left]:top-1/2! data-[side=right]:top-1/2! data-[side=left]:-right-1 data-[side=top]:-bottom-2.5 data-[side=right]:-left-1 data-[side=left]:-translate-y-1/2 data-[side=right]:-translate-y-1/2',
+        className,
+      )}
+      data-slot="tooltip-arrow"
+      {...props}
+    />
+  )
+}
+
 function TooltipContent({
   className,
   side = 'top',
@@ -77,4 +128,13 @@ function TooltipContent({
   )
 }
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
+export {
+  Tooltip,
+  TooltipArrow,
+  TooltipContent,
+  TooltipPopup,
+  TooltipPortal,
+  TooltipPositioner,
+  TooltipProvider,
+  TooltipTrigger,
+}
